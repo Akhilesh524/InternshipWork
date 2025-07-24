247a05e5519f50236b927d9e5aff959a9b54ab74
Akhilesh Bairwa <a9977134203@gmail.com>
2025-07-25 00:32:09 +0530

    debugging

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+   public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+   public static int sub(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int mul(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int div(int a, int b)
+    {
+        return b / a;
+    }
+    static void Main(string[] args)
+    {
+        int a = 4;
+        int b = 3;
+
+        Console.WriteLine("Addition"+add(a, b));
+
+        Console.WriteLine("subtraction" + sub(a, b));
+
+        Console.WriteLine("mul" + mul(a, b));
+
+        Console.WriteLine("div" + div(a, b));
+
+   
+    }
+
+   
+}
